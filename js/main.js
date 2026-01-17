16b0dc10417777d3c829dda763e7b3a92a593074
urichh <urban.dopudja@gmail.com>
2026-01-17 23:02:40 +0100

    added age field

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const age = new Date().getFullYear() - 2004;
+document.getElementById("age").textContent = age
+
+const ageField = document.getElementById("age");
+
+ageField.addEventListener("mouseenter", () => {
+    ageField.textContent = "Date().getFullYear() - 2004;";
+});
+
+ageField.addEventListener("mouseleave", () => {
+    ageField.textContent = age;
+});
